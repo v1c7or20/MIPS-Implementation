a4172d55875e18aebfba573a0b8c1e9d478114a1
victor <victor.pena@utec.edu.oe>
2020-06-21 21:31:11 -0500

    Ya esta, falta hacer que el formato final coincida

--- memfile_MIPS.asm
+++ memfile_MIPS.asm
@@ -1,18 +1,20 @@
-1 	ADDI $v0 $zero 0x0005
-2	ADDI $v1 $zero 0x000C
-3	ADDI $a3 $v1 0xFFF7
-4	OR $a0 $a3 $v0		#Posible error
-5	AND $a1 $v1 $a0
-6	ADD $a1 $a1 $a0
-7	BEQ $a1 $a3 0x000A
-8	SLT $a0 $v1 $a0
-9	BEQ $a0 $zero 0x0001
-10	ADDI $a1 $zero 0x0000
-11	SLT $a0 $a3 $v0
-12	ADD $a3 $a0 $a1
-13	SUB $a3 $a3 $v0
-14	SW $a3 0x0044 $v1
-15	LW $v0 0x0050 $zero
-16	J 0x0000011
-17	ADDI $v0 $zero 0x0001
-18	SW $v0 0x0054 $zero
+1 	ADDI $v0 $zero 0x0005	#v0 = 5
+2	ADDI $v1 $zero 0x000C	#v1 = c
+3	ADDI $a3 $v1 0xFFF7	#a3 = 3
+4	OR $a0 $a3 $v0		#a0 = 7
+5	AND $a1 $v1 $a0		#a1 = 4
+6	ADD $a1 $a1 $a0		#a1 = 7
+7	BEQ $a1 $a3 0x000A  	#No tomar
+8	SLT $a0 $v1 $a0		#a0 = 0
+9	BEQ $a0 $zero 0x0001	#No tomar
+A	ADDI $a1 $zero 0x0000	#a1 = 0
+B	SLT $a0 $a3 $v0		#a0 = 1
+C	ADD $a3 $a0 $a1		#a3 = 0
+D	SUB $a3 $a3 $v0		#a3 = FFFB
+E	SW $a3 0x0044 $v1	#nada
+F	LW $v0 0x0050 $zero	#v0 = 03FB
+10	J 0x0000011		#nada
+11	ADDI $v0 $zero 0x0001	#v0 = 1
+12	SW $v0 0x0054 $zero	#nada
+
+
