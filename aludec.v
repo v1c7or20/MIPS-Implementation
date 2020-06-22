c81336f6b0b0daf760595626b8b8c723615f1907
Renzo <renzo.tenazoa@utec.edu.pe>
2020-06-22 01:46:34 -0500

    Resto de instrucciones del ALU

--- aludec.v
+++ aludec.v
@@ -1,9 +1,8 @@
-
 module aludec(input  [5:0] funct,
               input  [1:0] aluop,
               output reg [3:0] alucontrol);
 
-  always @ (*)
+  always @ (*) begin
     case(aluop)
       2'b00: alucontrol <= 4'b0000;  // add
       2'b01: alucontrol <= 4'b0010;  // sub
@@ -12,8 +11,11 @@ module aludec(input  [5:0] funct,
           6'b100010: alucontrol <= 4'b0010; // SUB
           6'b100100: alucontrol <= 4'b0100; // AND
           6'b100101: alucontrol <= 4'b0101; // OR
+          6'b100110: alucontrol <= 4'b0110; // XOR
+          6'b100111: alucontrol <= 4'b0111; // NOR
           6'b101010: alucontrol <= 4'b1010; // SLT
           default:   alucontrol <= 4'bxxxx; // ???
         endcase
     endcase
+  end
 endmodule
